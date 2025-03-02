7914d36df63ca3d29513a98b02bd0444ee9a69ed
Nathan Perrier <nathanperrier138@gmail.com>
2025-03-02 16:38:47 +1000

    Add device.js module and include it in index.html for device information logging

---
+++ src/js/device.js
@@ -0,0 +1,9 @@
+console.log(window.app.device);
+
+console.log(window.app.device.os);
+console.log(window.app.device.browser);
+
+console.log(window.app.device.ios);
+console.log(window.app.device.android);
+console.log(window.app.device.desktop);
+
