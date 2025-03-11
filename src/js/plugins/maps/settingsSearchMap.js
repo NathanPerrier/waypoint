735f5312decbf961eb5feb31c66d2fd82751cfef
Nathan Perrier <nathanperrier138@gmail.com>
2025-03-11 17:50:38 +1000

    Refactor map initialization to execute on DOMContentLoaded and improve marker handling in settings map

--- src/js/plugins/maps/settingsSearchMap.js
+++ src/js/plugins/maps/settingsSearchMap.js
@@ -1,55 +1,60 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibmF0aGFuLXBlcnJpZXIyMyIsImEiOiJjbG8ybW9pYnowOTRiMnZsZWZ6NHFhb2diIn0.NDD8iEfYO1t9kg6q_vkVzQ';
+document.addEventListener('DOMContentLoaded', () => {
 
-const mapContainer = document.getElementById('settings-map');
+    mapboxgl.accessToken = 'pk.eyJ1IjoibmF0aGFuLXBlcnJpZXIyMyIsImEiOiJjbG8ybW9pYnowOTRiMnZsZWZ6NHFhb2diIn0.NDD8iEfYO1t9kg6q_vkVzQ';
 
-var map = new mapboxgl.Map({
-    container: mapContainer, //id element html
-    style: 'mapbox://styles/mapbox/light-v11',
-    zoom: 9 // starting zoom
-});
+    const mapContainer = document.getElementById('settings-map');
 
-var geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl,
-    marker: false,
-    zoom: 20
-});
+    console.log(mapContainer);
 
+    var map = new mapboxgl.Map({
+        container: mapContainer, //id element html
+        style: 'mapbox://styles/mapbox/light-v11',
+        zoom: 9 // starting zoom
+    });
 
-map.addControl(
-    geocoder
-);
-
-let marker = null
-map.on('click', function (e) {
-    mapClickFn(e.lngLat);
-
-    if (marker == null) {
-        marker = new mapboxgl.Marker()
-            .setLngLat(e.lngLat)
-            .addTo(map);
-    } else {
-        marker.setLngLat(e.lngLat)
-    }
-});
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false,
+        zoom: 20
+    });
 
 
+    map.addControl(
+        geocoder
+    );
 
-function mapClickFn(coordinates) {
-    const url =
-        "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-        coordinates.lng +
-        "," +
-        coordinates.lat +
-        ".json?access_token=" +
-        mapboxgl.accessToken
+    let marker = null
+    map.on('click', function (e) {
+        mapClickFn(e.lngLat);
 
-    fetch(url).then(res => res.json()).then((data) => {
-        if (data.features.length > 0) {
-            const address = data.features[0].place_name;
-            document.getElementById("startLocation").value = address;
+        if (marker == null) {
+            marker = new mapboxgl.Marker()
+                .setLngLat(e.lngLat)
+                .addTo(map);
         } else {
-            document.getElementById("startLocation").value = "No address found";
+            marker.setLngLat(e.lngLat)
         }
     });
-}
+
+
+
+    function mapClickFn(coordinates) {
+        const url =
+            "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+            coordinates.lng +
+            "," +
+            coordinates.lat +
+            ".json?access_token=" +
+            mapboxgl.accessToken
+
+        fetch(url).then(res => res.json()).then((data) => {
+            if (data.features.length > 0) {
+                const address = data.features[0].place_name;
+                document.getElementById("startLocation").value = address;
+            } else {
+                document.getElementById("startLocation").value = "No address found";
+            }
+        });
+    }
+});
