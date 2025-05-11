7e9bb721c8e625b0b788026d4259274665b8c082
NathanPerrier <nathanperrier138@gmail.com>
2025-05-11 16:42:12 +1000

    Implement code changes to enhance functionality and improve performance

--- src/js/plugins/locar/locarSuggestions.js
+++ src/js/plugins/locar/locarSuggestions.js
@@ -1,2 +1,4 @@
 // logic to retrieve sugestions and get make three.js objects and find their relative position on 
 // screen to be then handed to route and nav to be displayed to scene
+
+// get locar instance
